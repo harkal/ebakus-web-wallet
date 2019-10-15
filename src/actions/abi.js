28ffb9fa760f58007ae196e8cadcd2c78f1e15db
Chris Ziogas <ziogaschr@gmail.com>
2019-10-15 12:36:43 +0300

    Abi loader memoize

--- src/actions/abi.js
+++ src/actions/abi.js
@@ -1,18 +1,24 @@
-import Vue from 'vue'
 import abiDecoder from 'abi-decoder'
+import axios from 'axios'
+import debounce from 'lodash/debounce'
+import memoize from 'lodash/memoize'
 
-const cachedAbis = {}
+const requestAbiWithCaching = memoize(async req => await axios.get(req))
+
+const cleanAbiCacheForUrl = debounce(function(url) {
+  requestAbiWithCaching.cache.delete(url)
+}, 2000)
 
 const getAbi = async contractAddress => {
-  let abi = cachedAbis[contractAddress]
+  let abi
+  const abiUrl = `${process.env.API_ENDPOINT}/abi/${contractAddress}`
 
-  if (!abi) {
-    abi = await loadContractAbiFromExplorer(contractAddress)
-    if (!abi) {
-      return false
-    }
-
-    cachedAbis[contractAddress] = abi
+  try {
+    const req = await requestAbiWithCaching(abiUrl)
+    abi = req.data
+  } catch (err) {
+    cleanAbiCacheForUrl(abiUrl)
+    return false
   }
 
   return abi
@@ -33,15 +39,6 @@ const decodeDataUsingAbi = async (contractAddress, data) => {
 const getValueForParam = (name, params) => {
   const obj = params.find(item => item.name === name)
   return obj && obj.value
-}
-
-const loadContractAbiFromExplorer = async address => {
-  try {
-    const res = await Vue.http.get(`${process.env.API_ENDPOINT}/abi/${address}`)
-    return res.data
-  } catch {
-    return
-  }
 }
 
 export { decodeDataUsingAbi, getValueForParam }
