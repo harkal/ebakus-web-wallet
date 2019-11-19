d947fabb92fc6b21dde830064ce821c6e93dfb38
Chris Ziogas <ziogaschr@gmail.com>
2019-11-19 11:34:14 +0200

    Add receive testnet warning

--- src/components/Receive.vue
+++ src/components/Receive.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="receive scroll-wrapper">
     <div class="wrapper">
+      <div class="testnet">
+        <h2>
+          Important!
+        </h2>
+        <h3>
+          This wallet is connected at an ebakus test network,
+          <strong>please don’t send ERC-20 EBK</strong> at this address or main
+          network EBK <strong>as they may get lost forever</strong>.
+        </h3>
+      </div>
+
       <h5>{{ publicAddress }}</h5>
       <button class="full" :class="{ active: justCopied }" @click="doCopy">
         {{ btnLabel }}
       </button>
-      <VueQRCodeComponent :text="publicAddress" class="qrcode" />
+      <VueQRCodeComponent :text="publicAddress" class="qrcode" size="240" />
       <GetFaucet />
     </div>
   </div>
@@ -64,16 +75,31 @@ export default {
       font-weight: 600;
     }
   }
+}
 
-  .qrcode {
-    overflow: hidden;
-    max-width: 240px;
+.qrcode {
+  overflow: hidden;
+  max-width: 240px;
+  padding: 10px 0px;
+}
 
-    ::v-deep img {
-      max-width: 240px;
-      width: 240px;
-      padding: 10px 0px;
-    }
+.scroll-wrapper {
+  overflow-y: auto;
+}
+
+.testnet {
+  margin: -20px -39px 0;
+  padding: 20px 39px;
+  background-color: #f3067c;
+
+  h2,
+  h3 {
+    color: #ffffff;
+    font-weight: 300;
+  }
+
+  h3 {
+    font-weight: 400;
   }
 }
 </style>
