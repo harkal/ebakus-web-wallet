0a13fdeb9b452fbb014ff0666cbd317cee80b578
Chris Ziogas <ziogaschr@gmail.com>
2019-12-11 15:34:55 +0200

    Change NODE_URL at dev.env

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,7 +3,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  NODE_URL: '"wss://node-test.ebakus.com"',
+  NODE_URL: '"wss://node.ebakus.com"',
   // NODE_URL: '"https://noderpc.ebakus.com"',
   // NODE_URL: '"ws://127.0.0.1:8546"',
 })
