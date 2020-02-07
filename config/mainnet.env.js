54065367542db40d58e4477345abeb5621ce1787
Chris Ziogas <ziogaschr@gmail.com>
2020-02-07 16:42:03 +0200

    Update config for mainnet release

--- config/mainnet.env.js
+++ config/mainnet.env.js
@@ -1,26 +1,14 @@
 module.exports = {
   NODE_ENV: '"production"',
 
-  // For release
-  // MAINNET_DEPLOYED_URL: '"https://wallet.ebakus.com"',
-  // MAINNET_NODE_URL: '"wss://ws.ebakus.com"',
-  // MAINNET_EXPLORER_URL: '"https://explorer.ebakus.com"',
-  // MAINNET_API_ENDPOINT: '"https://explorersrv.ebakus.com"',
+  MAINNET_DEPLOYED_URL: '"https://wallet.ebakus.com"',
+  MAINNET_NODE_URL: '"wss://ws.ebakus.com"',
+  MAINNET_EXPLORER_URL: '"https://explorer.ebakus.com"',
+  MAINNET_API_ENDPOINT: '"https://explorer-api.ebakus.com"',
 
-  // TESTNET_NODE_URL: '"wss://ws.ebakus-testnet.com"',
-  // TESTNET_EXPLORER_URL: '"https://explorer.ebakus-testnet.com"',
-  // TESTNET_API_ENDPOINT: '"https://explorer-api.ebakus-testnet.com"',
-
-  // Temp data for testing
-  MAINNET_DEPLOYED_URL: '"https://wallet.ebakus-testnet.com"',
-  MAINNET_NODE_URL: '"wss://ws.ebakus-testnet.com"',
-  MAINNET_EXPLORER_URL: '"https://explorer.ebakus-testnet.com"',
-  MAINNET_API_ENDPOINT: '"https://explorer-api.ebakus-testnet.com"',
-
-  TESTNET_NODE_URL: '"wss://node.ebakus.com"',
-  TESTNET_EXPLORER_URL: '"https://explorer.ebakus.com"',
-  TESTNET_API_ENDPOINT: '"https://explorersrv.ebakus.com"',
-  // End - Temp data for testing
+  TESTNET_NODE_URL: '"wss://ws.ebakus-testnet.com"',
+  TESTNET_EXPLORER_URL: '"https://explorer.ebakus-testnet.com"',
+  TESTNET_API_ENDPOINT: '"https://explorer-api.ebakus-testnet.com"',
 
   MAINNET_CHAIN_ID: 10,
   TESTNET_CHAIN_ID: 7,
